e9fc5ee92660a1ed0f2de22ab259bb5204207cd5
MateuszFabisiewicz <01151578@pw.edu.pl>
2022-11-20 12:35:41 +0100

    Create new project

---
+++ MetricsCalculations/MetricsCalculations/FamilyOfSets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsCalculations
+{
+    public class FamilyOfSets
+    {
+        private List<List<int>> family;
+        
+        public List<List<int>> Family
+        {
+            get { return family; }
+            set { family = value; }
+        }
+
+        public FamilyOfSets() { 
+            family = new List<List<int>>();
+        }
+
+        public FamilyOfSets(string path)
+        {
+            family = new List<List<int>>();
+            Load(path);
+        }
+
+        private void Load(string path)
+        {
+            
+        }
+    }
+}
